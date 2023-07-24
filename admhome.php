2684ff41f4069dfee3a0236fd2f1c8c083d4199f
sarinha1403 <sarah.fontanezi14@gmail.com>
2023-07-24 11:41:03 -0300

    CRIAÇÃO DE ALETRAÇÃO DE PRODUTOS
    CRIAÇÃO DA TELA DO ALTERA PRODUTOS E CRIAÇÃO DA AREA DE CLIENTE ( TELA DE LOGIN), FALTA FAZER LOGOUT E LOJA.

--- admhome.php
+++ admhome.php
@@ -15,8 +15,9 @@
     <div>
         <ul class="menu">
             <li><a href="cadastrausuario.php">CADASTRA USUARIO</a></li>
-            <li><a href="listausuario.php">LISTA USUARIO</a></li>
+            <li><a href="cadastracliente.php">CADASTRA CLIENTE</a></li>
             <li><a href="cadastraproduto.php">CADASTRA PRODUTO</a></li>
+            <li><a href="listausuario.php">LISTA USUARIO</a></li>
             <li><a href="listaproduto.php">LISTA PRODUTO</a></li>
             <li><a href="listacliente.php">LISTA CLIENTE</a></li>
             <li class="menuloja"><a href="logout.php">SAIR</a></li>
